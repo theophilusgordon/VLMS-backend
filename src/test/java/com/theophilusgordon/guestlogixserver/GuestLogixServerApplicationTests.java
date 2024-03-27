e03345d9ad3947cd7a6b72e2692e921b5c4270c8
Theophilus Gordon <theophilus.gordon@amalitech.com>
2024-03-27 16:14:23 +0000

    feat: initial setup

---
+++ src/test/java/com/theophilusgordon/guestlogixserver/GuestLogixServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theophilusgordon.guestlogixserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuestLogixServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
