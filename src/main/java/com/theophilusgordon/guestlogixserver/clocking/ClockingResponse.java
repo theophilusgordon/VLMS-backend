d615963619f72d91065eb7126dbebec06b6d6f97
Theophilus Gordon <gordonfiifi@gmail.com>
2024-06-11 16:25:00 +0000

    refactor: clocking response as json properties

--- src/main/java/com/theophilusgordon/guestlogixserver/clocking/ClockingResponse.java
+++ src/main/java/com/theophilusgordon/guestlogixserver/clocking/ClockingResponse.java
@@ -1,5 +1,6 @@
 package com.theophilusgordon.guestlogixserver.clocking;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,9 +9,14 @@ import lombok.Setter;
 @Setter
 @Builder
 public class ClockingResponse {
+    @JsonProperty("id")
     private Integer id;
+    @JsonProperty("userId")
     private String userId;
+    @JsonProperty("workLocation")
     private String workLocation;
+    @JsonProperty("clockInDateTime")
     private String clockInDateTime;
+    @JsonProperty("clockOutDateTime")
     private String clockOutDateTime;
 }
