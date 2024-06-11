eaaea7f7b5958d8a4c91543721a225e5d3f135cd
Theophilus Gordon <gordonfiifi@gmail.com>
2024-06-11 16:39:53 +0000

    feat: qr code service test

---
+++ src/test/java/com/theophilusgordon/guestlogixserver/utils/QrCodeServiceTest.java
@@ -0,0 +1,32 @@
+package com.theophilusgordon.guestlogixserver.utils;
+
+import com.google.zxing.WriterException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class QRCodeServiceTest {
+
+    @InjectMocks
+    private QRCodeService qrCodeService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGenerateQRCodeImage() throws WriterException, IOException {
+        String qrCodeText = "Test QR Code";
+        byte[] qrCodeImage = qrCodeService.generateQRCodeImage(qrCodeText);
+
+        assertNotNull(qrCodeImage);
+        assertTrue(qrCodeImage.length > 0);
+    }
+}
