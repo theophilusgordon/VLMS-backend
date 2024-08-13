bf9538c2c77a09ddaa195dc356e4d9e13eaf8dc1
Theophilus Gordon <gordonfiifi@gmail.com>
2024-08-13 13:45:48 +0000

    feat: overload not found exception

--- src/main/java/com/theophilusgordon/vlmsbackend/exception/NotFoundException.java
+++ src/main/java/com/theophilusgordon/vlmsbackend/exception/NotFoundException.java
@@ -1,7 +1,18 @@
 package com.theophilusgordon.vlmsbackend.exception;
 
+import java.util.UUID;
+
 public class NotFoundException extends RuntimeException {
     public NotFoundException(String resource, String id) {
         super(resource + " with id " + id + " not found");
     }
+
+    public NotFoundException(String resource, int id) {
+        super(resource + " with id " + id + " not found");
+    }
+
+    public NotFoundException(String resource, UUID id) {
+        super(resource + " with id " + id.toString() + " not found");
+    }
 }
+
